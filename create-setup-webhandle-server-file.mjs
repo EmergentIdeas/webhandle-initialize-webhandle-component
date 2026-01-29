a4b5f681c540785025826b256ebc0f7fa703202c
Dan Kolz <dan@emergentideas.com>
2026-01-28 20:41:16 -0600

    create local file

---
+++ create-setup-webhandle-server-file.mjs
@@ -0,0 +1,27 @@
+#! /usr/local/bin/node
+import fs from "node:fs"
+import path from "node:path"
+
+let filename = 'setup-webhandle-server.mjs'
+
+let cwd = process.cwd()
+let packageDir = import.meta.dirname
+let outputPath = path.join(cwd, filename)
+let sourceFile = path.join(packageDir, filename)
+fs.stat(outputPath, (err, stats) => {
+	if(err || !stats) {
+		fs.readFile(sourceFile, (err, data) => {
+			if(!err) {
+				data = data.toString()
+				data = data.split('"./').join('"@webhandle/setup-webhandle-server/')
+				fs.writeFile(outputPath, data, (err) => {
+
+				})
+			}
+			else {
+				console.error(err)
+			}
+		})
+
+	}
+})
